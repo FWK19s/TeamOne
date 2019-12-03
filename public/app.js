1b75fc6b74fecb6fca3ed6aa8deac6cedb644ecd
Aleksander Dimitrijevic <dimitrijevicaleksander.dev@gmail.com>
2019-12-03 13:38:32 +0100

    finished the validation app-js

--- public/app.js
+++ public/app.js
@@ -19,21 +19,28 @@ function validateForm(e) {
       if (input.value.length == 0 && required.includes(fieldName)) {
         addError(input, "is required", fieldName);
         error = true;
-      } else if (fieldName == "fname" || fieldName == "lname") {
+      } else if (fieldName == "firstname" || fieldName == "lastname") {
         let checkForNumbers = /^[0-9]*$/;
         let result = checkForNumbers.test(input.value);
-        if (!result) {
+        if (result) {
           addError(input, "can't contain any numbers", fieldName);
           error = true;
         }
       } else if (fieldName == "phone") {
         let checkForNumbers = /^[0-9]*$/;
         let result = checkForNumbers.test(input.value);
-        if (input.value.length < 10 || input.value.length > 10) {
+        if (!result) {
+          addError(input, "number is not valid", fieldName);
+          error = true;
+        } else if (input.value.length < 10 || input.value.length > 10) {
           addError(input, "number can only be 10 characters long", fieldName);
           error = true;
-        } else if (!result) {
-          addError(input, "number is not valid", fieldName);
+        }
+      } else if (fieldName == "email") {
+        let checkValidEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+        let result = checkValidEmail.test(input.value);
+        if (!result) {
+          addError(input, "provided is not a valid e-mail address", fieldName);
           error = true;
         }
       }
